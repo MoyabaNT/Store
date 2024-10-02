7940e00f97e5167aa20a78761fec37eaf3c5cf41
MoyabaNT <155981478+MoyabaNT@users.noreply.github.com>
2024-10-02 22:01:42 +0200

    fixed names

--- online-store/src/afterSignIn/Cart.jsx
+++ online-store/src/afterSignIn/Cart.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import UserNavBar from '../Components/UserNavBar'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div>
+      <div className='flex items-center justify-center mt-40'>
+        <h1 className='shadow-sm shadow-purple-400 p-4'>Add items to cart to view cart</h1>
+      </div>
+      <UserNavBar />
+    </div>
   )
 }
 
